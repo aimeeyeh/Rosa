c173c3d4b03a1fb375031d35d5bff80a1212dee6
Chihchen Yeh <yehchihchen@gmail.com>
2021-05-13 01:09:49 +0800

    Completed basic storyboard flow and calender view

---
+++ Rosa/Controller/Record/RecordDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RecordDetailViewController.swift
+//  Rosa
+//
+//  Created by Chih-Chen Yeh on 2021/5/12.
+//
+
+import UIKit
+
+class RecordDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
