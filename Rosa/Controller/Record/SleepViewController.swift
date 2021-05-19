cf6ad937c9820669312c718e5104fdbe3e924889
Chihchen Yeh <yehchihchen@gmail.com>
2021-05-19 11:26:51 +0800

    implemented scroll view for photos in HomePage

---
+++ Rosa/Controller/Record/SleepViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SleepViewController.swift
+//  Rosa
+//
+//  Created by Chih-Chen Yeh on 2021/5/19.
+//
+
+import UIKit
+
+class SleepViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
