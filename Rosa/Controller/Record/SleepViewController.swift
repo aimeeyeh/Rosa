fdfb312c5ad9c423c25fce9d44adbbab2cfedbb3
Chihchen Yeh <yehchihchen@gmail.com>
2021-05-19 14:21:44 +0800

    implemented water popup view in DailyRecordPage

--- Rosa/Controller/Record/SleepViewController.swift
+++ Rosa/Controller/Record/SleepViewController.swift
@@ -15,7 +15,6 @@ class SleepViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
--- Rosa/Controller/Record/WaterViewController.swift
+++ Rosa/Controller/Record/WaterViewController.swift
@@ -8,21 +8,71 @@
 import UIKit
 
 class WaterViewController: UIViewController {
+    
+    let blackView = UIView(frame: UIScreen.main.bounds)
+    var glassAmount = 0 {
+        didSet {
+            let waterAmount = glassAmount * 250
+            waterLabel.text = "\(waterAmount) ml / 2500 ml"
+        }
+    }
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var waterLabel: UILabel!
+    
+    @IBAction func tappedGlass(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        sender.checkButtonState()
+        if sender.isSelected {
+            glassAmount += 1
+        } else {
+            glassAmount -= 1
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        let waterAmount = glassAmount * 250
+        waterLabel.text = "\(waterAmount) ml / 2000 ml"
 
-        // Do any additional setup after loading the view.
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func addBlackView() {
+        
+        blackView.backgroundColor = .black
+        blackView.alpha = 0
+        blackView.tag = 100
+        presentingViewController?.view.addSubview(blackView)
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0) {
+            self.blackView.alpha = 0.5
+            
+        }
     }
-    */
+    
+    func configureView() {
+        
+        addBlackView()
+        popUpView.layer.cornerRadius = 15
+        cancelButton.clipsToBounds = true
+        cancelButton.layer.cornerRadius = 15
+        cancelButton.layer.maskedCorners = [.layerMinXMaxYCorner]
+        saveButton.clipsToBounds = true
+        saveButton.layer.cornerRadius = 15
+        saveButton.layer.maskedCorners = [.layerMaxXMaxYCorner]
+        // Top right corner, Top left corner respectively
+        
+    }
+    
+    @IBAction func cancelWater(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        blackView.removeFromSuperview()
+    }
+    @IBAction func confirmWater(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        blackView.removeFromSuperview()
+    }
 
 }
