4f226243deb2eab82b893d05a74c4e65191c4db9
Chihchen Yeh <yehchihchen@gmail.com>
2021-05-20 13:32:53 +0800

    implemented basic UI in ProfilePage

--- Rosa/Controller/Profile/ProfileViewController.swift
+++ Rosa/Controller/Profile/ProfileViewController.swift
@@ -8,9 +8,58 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var postedButton: UIButton!
+    @IBOutlet weak var underlineView: UIView!
+    @IBOutlet weak var likedButton: UIButton!
+    
+    var currentType = "postedArticles"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+            postedButton.isSelected = false
+            likedButton.isSelected = false
+            sender.isSelected = !sender.isSelected
+
+            UIView.animate(withDuration: 0.3) {
+                self.underlineView.center.x = sender.center.x + 16
+            }
+
+            if sender.isSelected {
+                switch sender {
+                case postedButton:
+                    currentType = "postedArticles"
+                default:
+                    currentType = "likedArticles"
+                }
+            }
+
+        }
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ArticleCollectionViewCell",
+                                                        for: indexPath) as? ArticleCollectionViewCell {
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+
+}
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 60) / 2
+        return CGSize(width: width, height: width * 1.3)
+    }
 }
