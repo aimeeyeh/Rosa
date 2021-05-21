a22b85fa54be2a58eba7342b8dc6d82a3949d3a7
Chihchen Yeh <yehchihchen@gmail.com>
2021-05-21 15:25:21 +0800

    implemented basic Firebase/Firestore setups

--- Rosa/AppDelegate.swift
+++ Rosa/AppDelegate.swift
@@ -8,14 +8,16 @@
 // swiftlint:disable all
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
