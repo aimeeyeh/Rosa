728274543aafdec2338afc8593d6fc791f80ff21
Chihchen Yeh <yehchihchen@gmail.com>
2021-06-16 22:17:40 +0800

    changed the run scheme to traditional Chinese and refactored some code in main view controller

--- Rosa/Controller/Home/MainViewController.swift
+++ Rosa/Controller/Home/MainViewController.swift
@@ -45,59 +45,27 @@ class MainViewController: UIViewController, ChartViewDelegate {
     @IBOutlet weak var rightAfter: UIImageView!
     @IBOutlet weak var rightNoComparisonLabel: UILabel!
     @IBOutlet weak var rightStackView: UIStackView!
-
-    func createPastSevenDays() -> [Date] {
-        var past7Days = [Date]()
-        
-        let today = Date()
-        let calendar = Calendar.current
-        let todayStartTime = calendar.startOfDay(for: today)
-        
-        for index in -6 ... 0 {
-            var endDateComponents = DateComponents()
-            endDateComponents.day = index
-            if let day = calendar.date(byAdding: endDateComponents, to: todayStartTime) {
-                past7Days.append(day)
-            }
-        }
-        return past7Days
-    }
     
     var sleepLineChartYValues: [ChartDataEntry] = []
+    
     var waterBarChartYValues: [ChartDataEntry] = []
-    
-    func setUpSleepChartData() {
-        for (index, sleep) in sleepArray.enumerated() {
-            sleepLineChartYValues.append(ChartDataEntry(x: Double(index+1), y: sleep))
-//            print("x: \(Double(index+1)), y: \(sleep)")
-        }
-        configureSleepLineChart()
-    }
-    
-    func setUpWaterChartData() {
-        for (index, water) in waterArray.enumerated() {
-            waterBarChartYValues.append(BarChartDataEntry(x: Double(index+1), y: Double(water*250)))
-//            print("x: \(Double(index+1)), y: \(water)")
-        }
-        configureWaterBarChart()
-    }
     
     var sleepArray: [Double] = []
 
     var waterArray: [Int] = []
     
     var last7DayRecords: [Record] = [] {
+        
         didSet {
             if last7DayRecords.count == 0 {
                 waterChartView.isHidden = true
                 sleepLineChartView.isHidden = true
-//                noWaterRecord.isHidden = false
-//                noSleepRecord.isHidden = false
+                
             } else {
+                
                 waterChartView.isHidden = false
                 sleepLineChartView.isHidden = false
-//                noWaterRecord.isHidden = true
-//                noSleepRecord.isHidden = true
+                
                 let past7Days = createPastSevenDays()
                 for day in past7Days {
                     let filteredRecords = last7DayRecords.filter { $0.date == day }
@@ -107,9 +75,9 @@ class MainViewController: UIViewController, ChartViewDelegate {
                     } else {
                         sleepArray.append(0.0)
                         waterArray.append(0)
-                        
                     }
                 }
+                
                 setUpSleepChartData()
                 setUpWaterChartData()
                 configurePhotoView()
@@ -142,51 +110,98 @@ class MainViewController: UIViewController, ChartViewDelegate {
     
     var deeplinkArticle: Article?
     
+    fileprivate func fetchDeeplinkArticle(_ query: String) {
+        let defaults = UserDefaults.standard
+        defaults.set(nil, forKey: "query")
+        ArticleManager.shared.fetchArticle(articleID: query) { [weak self] result in
+            
+            switch result {
+            
+            case .success(let article):
+                
+                self?.deeplinkArticle = article
+                self?.showArticleFromDeeplink()
+                
+            case .failure(let error):
+                
+                print("fetchData.failure: \(error)")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
         noWaterRecord.isHidden = true
         noSleepRecord.isHidden = true
-        
         if let query = UserDefaults.standard.string(forKey: "query") {
-            let defaults = UserDefaults.standard
-            defaults.set(nil, forKey: "query")
-            ArticleManager.shared.fetchArticle(articleID: query) { [weak self] result in
-                
-                switch result {
-                
-                case .success(let article):
-                    
-                    self?.deeplinkArticle = article
-                    self?.openArticleDetail()
-                    
-                case .failure(let error):
-                    
-                    print("fetchData.failure: \(error)")
-                }
-            }
-            
+            fetchDeeplinkArticle(query)
         }
-                
-}
+        
+    }
     
     override func viewWillAppear(_ animated: Bool) {
+        
         self.resetData()
         fetchChallenge(date: Date())
         fetchPreviousRecords()
         configureViews()
         configureProgressView()
         configurePhotoView()
-
+        
     }
     
-    func openArticleDetail() {
+    func createPastSevenDays() -> [Date] {
+        
+        var past7Days = [Date]()
+        
+        let today = Date()
+        let calendar = Calendar.current
+        let startOfToday = calendar.startOfDay(for: today)
+        
+        for index in -6 ... 0 {
+            var endDateComponents = DateComponents()
+            endDateComponents.day = index
+            if let day = calendar.date(byAdding: endDateComponents, to: startOfToday) {
+                past7Days.append(day)
+            }
+        }
+        return past7Days
+    }
+    
+    func setUpSleepChartData() {
+        for (index, sleep) in sleepArray.enumerated() {
+            sleepLineChartYValues.append(ChartDataEntry(x: Double(index+1), y: sleep))
+        }
+        configureSleepLineChart()
+    }
+    
+    func setUpWaterChartData() {
+        for (index, water) in waterArray.enumerated() {
+            waterBarChartYValues.append(BarChartDataEntry(x: Double(index+1), y: Double(water*250)))
+        }
+        configureWaterBarChart()
+    }
+    
+    func showArticleFromDeeplink() {
+        
             if let articleDetailVC = UIStoryboard(name: "Articles", bundle: nil)
-                .instantiateViewController(withIdentifier: "ArticleDetailViewController") as? ArticleDetailViewController {
+                .instantiateViewController(
+                    withIdentifier: "ArticleDetailViewController") as? ArticleDetailViewController {
                 guard let deeplinkArticle = deeplinkArticle else { return }
                 articleDetailVC.article = deeplinkArticle
                 self.navigationController?.pushViewController(articleDetailVC, animated: true)
             }
+        
+    }
+        
+    func configureViews() {
+        challengeProgressView.shadowDecorate()
+        waterProgressView.shadowDecorate()
+        sleepProgressView.shadowDecorate()
+        priceView.shadowDecorate()
+        photoComparisonView.shadowDecorate()
     }
     
     func resetData() {
@@ -194,6 +209,126 @@ class MainViewController: UIViewController, ChartViewDelegate {
         self.waterArray = []
         self.sleepLineChartYValues = []
         self.waterBarChartYValues = []
+    }
+    
+    func configureProgressView() {
+        
+        ringProgressView.startColor = UIColor(red: 1.00, green: 0.84, blue: 0.64, alpha: 1.00)
+        ringProgressView.endColor = .orange
+        ringProgressView.backgroundRingColor = UIColor(red: 1.00, green: 0.84, blue: 0.64, alpha: 0.5)
+        ringProgressView.ringWidth = 14
+        ringProgressView.hidesRingForZeroProgress = true
+        ringProgressView.gradientImageScale = 0.5
+        ringProgressView.shadowOpacity = 0.0
+        ringProgressView.allowsAntialiasing = false
+        ringProgressView.style = .round
+        
+    }
+    
+    func configureWaterBarChart() {
+        
+        let dataSet = BarChartDataSet(entries: waterBarChartYValues, label: "Water (ml)".localized())
+        dataSet.drawValuesEnabled = false
+        
+        waterChartView.data = BarChartData(dataSets: [dataSet])
+        
+        let dateAxis = waterChartView.xAxis
+        dateAxis.drawGridLinesEnabled = false
+        dateAxis.labelPosition = .bottom
+        dateAxis.labelFont = UIFont.systemFont(ofSize: 7)
+        dateAxis.valueFormatter = MyXAxisFormatter()
+        
+        let waterAxis = waterChartView.rightAxis
+        waterAxis.drawGridLinesEnabled = false
+        waterAxis.granularityEnabled = true
+        waterAxis.granularity = 750
+        
+        let topAxis = waterChartView.leftAxis
+        topAxis.drawGridLinesEnabled = false
+        topAxis.drawLabelsEnabled = false
+        topAxis.drawAxisLineEnabled = false
+        
+        waterChartView.notifyDataSetChanged()
+        waterChartView.animate(yAxisDuration: 2.0)
+        
+    }
+    
+    func configureSleepLineChart() {
+        
+        let dataSet = LineChartDataSet(entries: sleepLineChartYValues, label: "Sleeping Hours".localized())
+        let sleepColor = UIColor.rgb(red: 178, green: 228, blue: 157, alpha: 1.0)
+        dataSet.drawCirclesEnabled = false
+        dataSet.drawValuesEnabled = false
+        dataSet.mode = .cubicBezier
+        dataSet.setColor(sleepColor)
+        dataSet.lineWidth = 3
+        dataSet.fill = Fill(color: sleepColor)
+        dataSet.fillAlpha = 0.8
+        dataSet.drawFilledEnabled = true
+
+        sleepLineChartView.data = LineChartData(dataSet: dataSet)
+        
+        let xAxis = sleepLineChartView.xAxis
+        xAxis.labelPosition = .bottom
+        xAxis.drawGridLinesEnabled = false
+        xAxis.labelFont = UIFont.systemFont(ofSize: 7)
+        xAxis.valueFormatter = MyXAxisFormatter()
+        
+        let yAxis = sleepLineChartView.leftAxis
+        yAxis.drawGridLinesEnabled = false
+        
+        sleepLineChartView.rightAxis.enabled = false
+        sleepLineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        
+    }
+    
+    // MARK: - firebase functions
+    
+    func fetchChallenge(date: Date) {
+        
+        ChallengeManager.shared.fetchChallenge(date: date) { [weak self] result in
+
+            switch result {
+
+            case .success(let challenges):
+
+                self?.challenges = challenges
+
+            case .failure(let error):
+
+                print("fetchData.failure: \(error)")
+            }
+        }
+    }
+    
+    func fetchPreviousRecords() {
+        
+        RecordManager.shared.fetchPast7daysRecords { [weak self] result in
+            
+            switch result {
+            
+            case .success(let records):
+                
+                self?.last7DayRecords = records
+                
+            case .failure(let error):
+                
+                print("fetchData.failure: \(error)")
+            }
+        }
+    }
+
+}
+
+// MARK: - Photo comparisons & ScrollViewDelegate
+
+extension MainViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        pageControl.currentPage = Int(page)
+        
     }
     
     func configurePhotoView() {
@@ -232,7 +367,7 @@ class MainViewController: UIViewController, ChartViewDelegate {
             
             let firstDayFrontalPhoto = last7DayRecords[0].fullPhoto
             let todayFrontalPhoto = last7DayRecords[6].fullPhoto
-            if firstDayFrontalPhoto == "" || todayFrontalPhoto == "" {
+            if firstDayFrontalPhoto.isEmpty || todayFrontalPhoto.isEmpty {
                 hideFrontalCard()
             } else {
                 frontalNoComparisonLabel.isHidden = true
@@ -247,7 +382,7 @@ class MainViewController: UIViewController, ChartViewDelegate {
             
             let firstDayLeftPhoto = last7DayRecords[0].leftPhoto
             let todayLeftPhoto = last7DayRecords[6].leftPhoto
-            if firstDayLeftPhoto == "" || todayLeftPhoto == "" {
+            if firstDayLeftPhoto.isEmpty || todayLeftPhoto.isEmpty {
                 hideLeftCard()
             } else {
                 leftNoComparisonLabel.isHidden = true
@@ -262,7 +397,7 @@ class MainViewController: UIViewController, ChartViewDelegate {
             
             let firstDayRightPhoto = last7DayRecords[0].rightPhoto
             let todayRightPhoto = last7DayRecords[6].rightPhoto
-            if firstDayRightPhoto == "" || todayRightPhoto == "" {
+            if firstDayRightPhoto.isEmpty || todayRightPhoto.isEmpty {
                 hideRightCard()
             } else {
                 rightNoComparisonLabel.isHidden = true
@@ -275,110 +410,7 @@ class MainViewController: UIViewController, ChartViewDelegate {
                 rightAfter.kf.setImage(with: URL(string: todayRightPhoto))
             }
         }
+        
     }
     
-    func configureViews() {
-        challengeProgressView.shadowDecorate()
-        waterProgressView.shadowDecorate()
-        sleepProgressView.shadowDecorate()
-        priceView.shadowDecorate()
-        photoComparisonView.shadowDecorate()
-    }
-    
-    func configureProgressView() {
-        ringProgressView.startColor = UIColor(red: 1.00, green: 0.84, blue: 0.64, alpha: 1.00)
-        ringProgressView.endColor = .orange
-        ringProgressView.backgroundRingColor = UIColor(red: 1.00, green: 0.84, blue: 0.64, alpha: 0.5)
-        ringProgressView.ringWidth = 14
-        ringProgressView.hidesRingForZeroProgress = true
-        ringProgressView.gradientImageScale = 0.5
-        ringProgressView.shadowOpacity = 0.0
-        ringProgressView.allowsAntialiasing = false
-        ringProgressView.style = .round
-    }
-    
-    func configureWaterBarChart() {
-        let dataSet = BarChartDataSet(entries: waterBarChartYValues, label: "Water (ml)".localized())
-        let data = BarChartData(dataSets: [dataSet])
-        waterChartView.data = data
-        waterChartView.xAxis.drawGridLinesEnabled = false
-        waterChartView.xAxis.labelPosition = .bottom
-        dataSet.drawValuesEnabled = false
-        let topAxis = waterChartView.leftAxis
-        topAxis.drawGridLinesEnabled = false
-        topAxis.drawLabelsEnabled = false
-        topAxis.drawAxisLineEnabled = false
-        waterChartView.rightAxis.drawGridLinesEnabled = false
-        waterChartView.rightAxis.granularityEnabled = true
-        waterChartView.rightAxis.granularity = 750
-        waterChartView.maxVisibleCount = 60
-        waterChartView.notifyDataSetChanged()
-        waterChartView.animate(yAxisDuration: 2.0)
-        waterChartView.xAxis.labelFont = UIFont.systemFont(ofSize: 7)
-        waterChartView.xAxis.valueFormatter = MyXAxisFormatter()
-    }
-    
-    func configureSleepLineChart() {
-        let sleepColor = UIColor.rgb(red: 178, green: 228, blue: 157, alpha: 1.0)
-        let set = LineChartDataSet(entries: sleepLineChartYValues, label: "Sleeping Hours".localized())
-        let data = LineChartData(dataSet: set)
-        set.drawCirclesEnabled = false
-        set.mode = .cubicBezier
-        set.setColor(sleepColor)
-        set.lineWidth = 3
-        set.fill = Fill(color: sleepColor)
-        set.fillAlpha = 0.8
-        set.drawFilledEnabled = true
-        data.setDrawValues(false)
-        sleepLineChartView.data = data
-        sleepLineChartView.rightAxis.enabled = false
-        sleepLineChartView.xAxis.labelPosition = .bottom
-        let yAxis = sleepLineChartView.leftAxis
-        yAxis.drawGridLinesEnabled = false
-//        yAxis.granularity = 0.5
-        sleepLineChartView.xAxis.drawGridLinesEnabled = false
-        sleepLineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
-        sleepLineChartView.xAxis.labelFont = UIFont.systemFont(ofSize: 7)
-        sleepLineChartView.xAxis.valueFormatter = MyXAxisFormatter()
-    }
-    
-    func fetchChallenge(date: Date) {
-        ChallengeManager.shared.fetchChallenge(date: date) { [weak self] result in
-
-            switch result {
-
-            case .success(let challenges):
-
-                self?.challenges = challenges
-
-            case .failure(let error):
-
-                print("fetchData.failure: \(error)")
-            }
-        }
-    }
-    
-    func fetchPreviousRecords() {
-        RecordManager.shared.fetchPast7daysRecords { [weak self] result in
-            
-            switch result {
-            
-            case .success(let records):
-                
-                self?.last7DayRecords = records
-                
-            case .failure(let error):
-                
-                print("fetchData.failure: \(error)")
-            }
-        }
-    }
-
-}
-
-extension MainViewController: UIScrollViewDelegate {
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let page = scrollView.contentOffset.x / scrollView.bounds.width
-        pageControl.currentPage = Int(page)
-    }
 }
