495ab12c39dcf8d133191d81ca20d791b40a3635
Chihchen Yeh <yehchihchen@gmail.com>
2021-06-19 23:18:34 +0800

    refactored models

--- Rosa/Model/User.swift
+++ Rosa/Model/User.swift
@@ -9,37 +9,11 @@ import Foundation
 import AuthenticationServices
 
 struct User: Codable {
-    
     let id: String
     let name: String
     var photo: String?
-//    var location: Location?
     var likedArticles: [String]?
     var followed: [String]?
     var followers: [String]?
     var blocklist: [String]?
-    
-//    init(credentials: ASAuthorizationAppleIDCredential) {
-//        self.id = credentials.user
-//        self.name = credentials.fullName?.givenName ?? ""
-//        self.photo = ""
-//        self.likedArticles = []
-//        self.followed = []
-//        self.followers = []
-//        self.blocklist = []
-//    }
-    
 }
-
-// struct Location: Codable {
-//
-//    var latitude: Double
-//    var longitude: Double
-//
-//    var toDict: [String: Any] {
-//        return [
-//            "latitude": latitude as Any,
-//            "longitude": longitude as Any
-//        ]
-//    }
-// }
